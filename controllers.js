0eeaeae40fab2d76d0b4931be479fb8f698905dc
marcos-gonzalez-castillo <marcos.gonzalez.castillo@gmail.com>
2014-12-04 15:17:33 +0100

    Create controllers.js

---
+++ controllers.js
@@ -0,0 +1,7 @@
+var phonecatApp = angular.module('phonecatApp', []);
+phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
+	$http.get('phones.json').success(function (data) {
+		$scope.phones = data;
+	});
+	$scope.orderProp = 'age';
+});
